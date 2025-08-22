29eda4ff100d51f31e88d039584e454b2028bafb
manyfaces-coder <semenovoleg99@yandex.ru>
2025-08-23 02:30:38 +0300

    Созданы отношения между user и post

--- H/core/models/post.py
+++ H/core/models/post.py
@@ -1,8 +1,11 @@
-from enum import unique
-
+from typing import TYPE_CHECKING
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
+
+if TYPE_CHECKING:  # спрашиваем идет ли проверка типов, а не выполнение кода
+    # (при этом импорт не происходит -> не будет циклических импортов)
+    from .user import User
 
 
 class Post(Base):
@@ -14,3 +17,6 @@ class Post(Base):
     # вручную, то default применится и там
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped[User] = relationship(
+        back_populates="posts"
+    )  # ссылаем на объект User и нужно указать, какое поле используем, чтобы попасть сюда с юзера
--- H/core/models/user.py
+++ H/core/models/user.py
@@ -1,8 +1,11 @@
-from enum import unique
+from typing import TYPE_CHECKING
 
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
+
+if TYPE_CHECKING:
+    from .post import Post
 
 
 class User(Base):
@@ -10,3 +13,4 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
     # Mapped указывает, что поле связано с колонкой таблицы в БД, mapped_column uniq - username уникальное,
     # String(32) – ограничение по длине
+    posts: Mapped[list[Post]] = relationship(back_populates="user")
