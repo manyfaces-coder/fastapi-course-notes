c368ecf2dc375b872d9fbc80a9bfd583a1e0c9e8
manyfaces-coder <semenovoleg99@yandex.ru>
2025-08-23 03:30:35 +0300

    Создание отношения между user и mixin

--- H/core/models/post.py
+++ H/core/models/post.py
@@ -1,22 +1,24 @@
-from typing import TYPE_CHECKING
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
-
-if TYPE_CHECKING:  # спрашиваем идет ли проверка типов, а не выполнение кода
-    # (при этом импорт не происходит -> не будет циклических импортов)
-    from .user import User
+from .mixins import UserRelationMixin
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_back_populates = "posts"
 
     title: Mapped[str] = mapped_column(String(100))
-    body: Mapped[str] = mapped_column(Text, default="", server_default="")
+    body: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
     # разница между default="" и server_default="", в первом случае применяется, когда мы создаем экземпляр
     # прямо в alchemy, а во втором сервером у нас выступает БД и если мы будем создавать запись прямо там
     # вручную, то default применится и там
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    user: Mapped[User] = relationship(
-        back_populates="posts"
-    )  # ссылаем на объект User и нужно указать, какое поле используем, чтобы попасть сюда с юзера
+    # Перенесли в mixins.py так как здесь и в profile.py повторяются
+    # user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    # user: Mapped[User] = relationship(
+    #     back_populates="posts"
+    # )  # ссылаем на объект User и нужно указать, какое поле используем, чтобы попасть сюда с юзера
